dcc62c4c45417e5a6497a92ac26c26f8305701a4
PatrickePatate <salut@lucienpuget.fr>
2024-03-24 18:00:29 +0100

    Fix HTTP Check edit bug

--- app/Filament/Resources/Checks/HttpCheckResource/Pages/EditHttpCheck.php
+++ app/Filament/Resources/Checks/HttpCheckResource/Pages/EditHttpCheck.php
@@ -18,10 +18,15 @@ class EditHttpCheck extends EditRecord
         $request_args = [];
         $headers = [];
         foreach(json_decode($data['request_args'],1) as $key => $d){
-            $request_args[] = ['key' => $key, "value" => $d];
+            if(!empty($key) && !empty($d)){
+                $request_args[] = ['key' => $key, "value" => $d];
+            }
         }
         foreach(json_decode($data['provide_headers'],1) as $key => $d){
-            $headers[] = ["header_key" => $key, "header_value" => $d];
+            if(!empty($key) && !empty($d)){
+                $headers[] = ["header_key" => $key, "header_value" => $d];
+            }
+
         }
         $data['provide_headers'] = $headers;
         $data['request_args'] = $request_args;
@@ -31,22 +36,13 @@ class EditHttpCheck extends EditRecord
 
     protected function handleRecordUpdate(Model $record, array $data): Model
     {
-        // processing arrays
-        $request_args = [];
-        $headers = [];
-//        foreach(json_decode($data['request_args'],1) as $d){
-//            $request_args[] = ['key' => $d['key'], "value" => $d['value']];
-//        }
-//        foreach(json_decode($data['provide_headers'],1) as $d){
-//            $headers[] = ["header_key" => $d['header_key'], "header_value" => $d['header_value']];
-//        }
-
+        
         $record->update([
             'url' => $data['url'],
             'method' => $data['method'],
-            'request_args' => $request_args,
+            'request_args' => $data['request_args'],
             'service_id' => $data['service_id'],
-            'provide_headers' => $headers,
+            'provide_headers' => $data['headers'],
             'http_code' => $data['http_code'],
             'http_body' => $data['http_body'],
             'check_cert' => $data['check_cert'],
